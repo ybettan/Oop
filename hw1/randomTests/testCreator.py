91cb19ec4b39cbe76d7ca6ded69f9fbeece7cbb1
Yoni Bettan <ybettan@redhat.com>
2018-04-15 12:17:51 +0300

    hw1/randomTests/testCreator.py : added PolyStream input and output files
    Signed-off-by: Yoni Bettan <ybettan@redhat.com>

--- hw1/randomTests/testCreator.py
+++ hw1/randomTests/testCreator.py
@@ -2,7 +2,10 @@ import random
 
 
 monomCmds = ['exp', 'exp:', 'coef', 'coef:', 'derivative']
-polynomCmds = ['addMonom:', 'multiplyByMonom:', 'asDictionary', 'derivative', 'eval:', 'add:']
+polynomCmds = ['addMonom:', 'multiplyByMonom:', 'asDictionary', 'derivative', \
+                'eval:', 'add:']
+polyStreamCmds = ['addCoefWithExp:', 'block', 'add:', 'substitute:', 'multiplyBy:', \
+        'filter:', 'intersectionWithWithinRange:', 'eval:']
 
 
 def command(cmd):
@@ -99,3 +102,67 @@ for i in range(5):
             printer('p eval: {}'.format(random.randint(-6, 6)))
 
 f.close()
+
+
+# PolyStream test
+f = open('polystream.in', 'w')
+
+for i in range(5):
+    command('p := PolyStream new')
+    for i in range(25):
+        cmd = random.choice(polyStreamCmds)
+        if (cmd == 'addCoefWithExp:'):
+            # check exception
+            floatNum = random.uniform(-6, 6)
+            exception('p addCoef: 6 withExp: {}'.format(floatNum))
+            exception('p addCoef: {}  withExp: 3'.format(floatNum))
+            # int nums
+            coef = random.randint(-6, 6)
+            exp = random.randint(-4, 4)
+            if (exp < 0):
+                exception('p addCoef: {} withExp: {}'.format(coef, exp))
+            else:
+                command('p addCoef: {} withExp: {}'.format(coef, exp))
+                printer('p eval: {}'.format(random.randint(-5, 5)))
+        elif (cmd == 'add:'):
+            # check exception
+            exception('p add: \'hello\'')
+            # create an argument polystream
+            command('q := PolyStream new')
+            for i in range(random.randint(0, 4)):
+                coef = random.randint(-6, 6)
+                exp = random.randint(0, 4)
+                command('q addCoef: {} withExp: {}'.format(coef, exp))
+        elif (cmd == 'substitute:' or cmd == 'multiplyBy'):
+            # check exception
+            exception('p ' + cmd + ' {}'.format(random.uniform(-6, 6)))
+            # check result
+            arg = random.randint(-6, 6)
+            if (cmd == 'substitute:' and arg == 0):
+                exception('p substitute: 0')
+            else:
+                printer('(p ' + cmd + ' {}) eval: {}'\
+                        .format(arg, random.randint(-6, 6)))
+        elif (cmd == 'filter:'):
+            # create a random set
+            command('s := Set new')
+            for i in range(random.randint(0, 4)):
+                command('s add: {}'.format(random.randint(-1, 5)))
+            # check result
+            printer('(p filter: s) eval: {}'.format(random.randint(-6, 6)))
+            # make sure future change on the set won't affect the polystream
+            command('s := Set new')
+            for i in range(random.randint(0, 4)):
+                command('s add: {}'.format(random.randint(-1, 5)))
+            printer('p eval: {}'.format(random.randint(-6, 6)))
+        else: # eval
+            # check exception
+            exception('p eval: {}'.format(random.uniform(-6, 6)))
+            # check result
+            printer('p eval: {}'.format(random.randint(-6, 6)))
+
+f.close()
+
+
+
+
