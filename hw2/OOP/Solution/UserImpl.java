2c1fc01c23798e471daa384dd1c691665d549447
Yoni Bettan <ybettan@redhat.com>
2018-05-02 09:38:09 +0300

    OOP/Solution/UserImpl.java : Done but NOT TESTED
    Signed-off-by: Yoni Bettan <ybettan@redhat.com>

--- hw2/OOP/Solution/UserImpl.java
+++ hw2/OOP/Solution/UserImpl.java
@@ -3,11 +3,12 @@ package OOP.Solution;
 import OOP.Provided.User;
 import OOP.Provided.Song;
 
-import java.util.stream;
+import java.util.stream.*;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.LinkedList;
 import java.util.HashMap;
-import java.util.Vector; //FIXME: remove
 
 public class UserImpl implements User {
 
@@ -15,15 +16,18 @@ public class UserImpl implements User {
     private String userName;
     private int userAge;    
     /* the songs is stored in a HashMap such that each entry has the form:
-     * songID -> rate */
-    private HashMap<Integer,Integer> songs;
+     * Song -> rate */
+    private HashMap<Song,Integer> songs;
+    /* all the user's friends are stored in a HashSet */
+    private HashSet<User> friends;
 
     public UserImpl(int userID, String userName, int userAge) {
         this.userID = userID;
         /* as mentioned in FAQ userName don't need to be cloned */
         this.userName = userName;
         this.userAge = userAge;
-        this.songs = new HashMap<Integer,Integer>();
+        this.songs = new HashMap<Song,Integer>();
+        this.friends = new HashSet<User>();
     }
 
     public int getID() {
@@ -46,36 +50,87 @@ public class UserImpl implements User {
                 throw new IllegalRateValue();
                 
             /* check if song was already rated */
-            if (songs.containsKey(song.getID()))
+            if (songs.containsKey(song))
                 throw new SongAlreadyRated();
 
             /* rate the sont */
-            songs.put(song.getID(), rate);
+            songs.put(song, rate);
 
             return this;
     }
 
     public double getAverageRating() {
-
-        double sum = songs.values().stream().sum();
+        double sum = songs.values().stream().reduce(0, Integer::sum);
         int counter = songs.values().size();
         return sum/counter;
     }
 
-    public int getPlaylistLength() {return 1;}
+    public int getPlaylistLength() {
+        return songs.keySet().stream().map(s -> s.getLength())
+                                      .reduce(0, Integer::sum); 
+    }
 
-    public Collection<Song> getRatedSongs() {return new Vector<Song>();}
+    public Collection<Song> getRatedSongs() {
+
+        /* we start from the last sort to keep a stable sort */
+        return songs.keySet().stream()
+                             .sorted((s1,s2) -> s1.getID() - s2.getID())     
+                             .sorted((s1,s2) -> s2.getLength() - s1.getLength())     
+                             .sorted((s1,s2) -> songs.get(s2) - songs.get(s1))
+                             .collect(Collectors.toCollection(LinkedList::new));
+    }
     
-    public Collection<Song> getFavoriteSongs() {return new Vector<Song>();}
+    public Collection<Song> getFavoriteSongs() {
 
-    public User AddFriend(User friend) throws AlreadyFriends , SamePerson {return new UserImpl(1, "yoni", 27);}
+        /* sort by the natural order (according to compareTo() ) */
+        return songs.keySet().stream().filter(x -> x.getLength() >= 8)
+            .sorted().collect(Collectors.toCollection(LinkedList::new));
+    }
 
-    public boolean favoriteSongInCommon(User user) {return true;}
+    public User AddFriend(User friend) throws AlreadyFriends , SamePerson {
 
-    public Map<User,Integer> getFriends() {return new HashMap<User,Integer>();}
+        /* ckeck if they are already friends */
+       if (friends.contains(friend)) 
+           throw new AlreadyFriends();
+
+       /* check it is not the same person.
+        * the userID is uniq so it is equivalent of testing (userID == friendID) */
+       if (friend == this)
+           throw new SamePerson();
+
+       /* add a new frendship relation */
+       friends.add(friend);
+
+       return this;
+    }
+
+    public boolean favoriteSongInCommon(User user) {
+        Collection<Song> tmp = new LinkedList<Song>(getFavoriteSongs());
+        tmp.retainAll(user.getFavoriteSongs());
+        return tmp.size() > 0;
+    }
+
+    public Map<User,Integer> getFriends() {
+        HashMap<User,Integer> res = new HashMap<User,Integer>(); 
+        for (User friend : friends) {
+            res.put(friend, friend.getRatedSongs().size());
+        }
+        return res;
+    }
 
     /* implement Comparable<User>.compareTo() */
-    public int compareTo(User other) {return 1;}
+    public int compareTo(User other) {
+        return getID() - other.getID();
+    }
+
+    /* override Object.equal */
+    @Override public boolean equals(Object o) {
+        if ( !(o instanceof User))
+            return false;
+
+        User other = (User)o;
+        return compareTo(other) == 0;
+    }
 }
 
 
